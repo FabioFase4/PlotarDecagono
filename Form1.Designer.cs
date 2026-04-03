8c1aee7986644cd8363ac2c9cbd2d7be51387d65
Fabio <seu-email@exemplo.com>
2026-04-03 18:03:06 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace projeto1_aula
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(941, 432);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 46);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1147, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 92);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Plotar Decágono";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1924, 533);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿/*Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ * Curso Técnico em Informática - Data de Entrega: 06 / 08 / 2026
+ * Autores do Projeto: Fábio Silva de Lima
+ *                     Luís Otávio Miranda
+ *
+ * Turma: 3I
+ * Atividade Proposta em aula
+ * Observação: <Uso de Pen.Dispose() para Limpeza de Memóriae Técnicas de Redução de Gasto de Memória>
+ * 
+ * 
+ * ******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto1_aula
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Random rnd = new Random();
+        private bool coresDefinidas = false;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (coresDefinidas)
+                Desenhar(e);
+        }
+
+        private void Desenhar (PaintEventArgs e)
+        {
+            int r = 0, g = 0, b = 0;
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    r = 255;
+                    break;
+
+                case 1:
+                    g = 255;
+                    break;
+
+                case 2:
+                    b = 255;
+                    break;
+
+                case 3:
+                    g = 255;
+                    b = 255;
+                    break;
+
+                case 4:
+                    r = 255;
+                    b = 255;
+                    break;
+
+                case 5:
+                    r = 255;
+                    g = 255;
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    r = 255;
+                    g = 255;
+                    b = 255;
+                    break; 
+            }
+
+            decagono(e, r, g, b);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!coresDefinidas)
+                DefinirCores();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!coresDefinidas)
+                coresDefinidas = true;
+
+            Invalidate();
+            MessageBox.Show("Decágono Gerado com Sucesso!");
+        }
+
+        public float Modulo(float a, float b)
+        {
+            return (a % b + b) % b;
+        }
+
+        private Pen CriarCaneta (int R, int G, int B)
+        {
+            return new Pen(Color.FromArgb(R, G, B));
+        }
+
+        private void DesenharPonto (PaintEventArgs e, Pen caneta, int x, int y)
+        {
+            e.Graphics.DrawLine(caneta, x, y, x + 1, y);
+        }
+
+        public void decagono(PaintEventArgs e, int r, int g, int b)
+        {
+            int n = 10; 
+            int R = 150; 
+            int tentativas = 80000;
+            int largura = 800;
+            int altura = 800;
+            int centroX = largura / 2;
+            int centroY = altura / 2;
+            int dx = 0, dy = 0;
+            int px = 0, py = 0;
+
+            float α = 0;
+            float seccao = (float)Math.PI * 2 / n;
+            float angulo = 0;
+
+            double d = 0;
+            double dmax = 0;
+            
+            Pen caneta = CriarCaneta (r, g, b);
+
+            for (int i = 0; i < tentativas; i++)
+            {
+                px = rnd.Next(centroX - R, centroX + R);
+                py = rnd.Next(centroY - R, centroY + R);
+
+                dx = px - centroX;
+                dy = py - centroY;
+
+                d = dx * dx + dy * dy;
+
+                if (d > R * R) 
+                    continue;
+
+                angulo = (float)Math.Atan2(dy, dx);
+                α = (float)(Modulo(angulo, seccao) - (seccao / 2.0));
+
+                dmax = R * Math.Cos(Math.PI/n) / Math.Cos(α);
+
+                if (d < dmax * dmax)
+                    DesenharPonto(e, caneta, px, py);
+            }
+
+            caneta.Dispose();
+        }
+
+        private void DefinirCores ()
+        {
+            string[] colors = {"Vermelho", "Verde", "Azul", "Ciano", "Magenta", "Amarelo", "Escuro", "Branco"};
+            foreach (string color in colors)
+            {
+                comboBox1.Items.Add(color);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
